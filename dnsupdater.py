51d400a6160a612d3a5ee8a3476f8885aa8c742e
Pritesh <me@pritesh.net>
2020-09-18 18:12:41 +0100

    IP address change checks added

--- dnsupdater.py
+++ dnsupdater.py
@@ -4,26 +4,42 @@ import os.path
 from os import path
 
 
-logging.basicConfig(filename='ip-changes.log',level=logging.INFO)
+logging.basicConfig(filename='dns-updater.log',level=logging.INFO)
 
 currentIp = None
 storedIpFile = '/root/stored-ip'
 
 
 def external_ip_changed():
-    currentIp  = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+    global currentIp  
+    currentIp = urllib.request.urlopen('https://ident.me').read().decode('utf8')
     print(currentIp)
    
     if( not path.exists(storedIpFile) ):
-        print(f"IP file {storedIpFile} does not exist")
+        logging.debug("IP file [{}] does not exist".format(storedIpFile))
+        with open(storedIpFile, "w") as ip_file:
+            ip_file.write(currentIp)
+        return True
     else:
         print("IP file exists")
+        with open(storedIpFile, "r") as ip_file:
+            storedIp = ip_file.read()
+
+        # Check if stored IP matches current external IP
+        if( storedIp == currentIp ):
+            print("IP Address unchanged")
+            return False
+        else:
+            with open(storedIpFile, "w") as ip_file:
+                ip_file.write(currentIp)
+            return True
+
+
 
   
+
 def updateDns():
-    pass 
-
-
+    print("DNS Update requested for: " + currentIp) 
 
 
 
